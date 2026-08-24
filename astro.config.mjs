b81c0b9cbd7919a1411a50abc48e445be55c03f9
302bis <achalay@gmail.com>
2026-08-24 01:43:00 -0600

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://302bis.github.io/photo',
+  base: '/photography-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
